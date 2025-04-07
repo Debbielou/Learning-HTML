1ee3bc837e4ab4c3dd62672470e825f82c616d68
Debbielou <denakamatte@gmail.com>
2025-04-07 07:12:34 +0300

    update on Learning HTML, Forms, & Responsive

---
+++ first.js
@@ -0,0 +1,10 @@
+const button = document.getElementById('myButton');
+
+button.addEventListener('click', function() {
+  alert('Button clicked!');
+  console.log("Button was clicked");
+});
+
+function message() {
+    alert("YESSS")
+}
